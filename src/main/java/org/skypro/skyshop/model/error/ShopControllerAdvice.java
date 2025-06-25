87542d52099db5a497f010ba5113371b78ffc34c
Far1ck <salyakhov.farit@gmail.com>
2025-06-25 21:02:49 +0500

    Homework Done

--- src/main/java/org/skypro/skyshop/model/error/ShopControllerAdvice.java
+++ src/main/java/org/skypro/skyshop/model/error/ShopControllerAdvice.java
@@ -2,14 +2,13 @@ package org.skypro.skyshop.model.error;
 
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ShopControllerAdvice {
     @ExceptionHandler(NoSuchProductException.class)
-    public ResponseEntity<ShopError> handleNoSuchProductException
-            (NoSuchProductException e) {
+    public ResponseEntity<ShopError> handleNoSuchProductException() {
         return new ResponseEntity<>(new ShopError("404", "No Such Product Found"), HttpStatusCode.valueOf(404));
     }
 }
